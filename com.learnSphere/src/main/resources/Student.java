7deee46c3dd5d77dc39d4eb208ad84ca33251aed
Swathiel01 <swathiel02@gmail.com>
2025-09-17 20:54:39 +0530

    Initial commit - Learn Sphere project

---
+++ com.learnSphere/src/main/resources/Student.java
@@ -0,0 +1,30 @@
+package 
+public class Student {
+	int rollno;
+	String name;
+	int marks;
+	
+	void study() {
+		System.out.println(name + "Studying");
+	}
+	void dance() {
+		System.out.println("name + "is an excellent dancer");
+		
+	}
+}
+	
+	class Main() {
+		public static void mai(String[] args) {
+			Student s1=new Student();
+			s1.rollno=14;
+			s1.name="Mona";
+			s1.study();
+			s1.dance();
+				
+				
+			}
+			
+		}
+	
+
+
